1e8591d092ccb31ba27583f52e6429c32723b20b
Soufiane-Majdar <soufianemjr3145@gmail.com>
2023-01-16 12:04:15 +0100

    Table view

--- src/View/Dashboard/CenterPanel.java
+++ src/View/Dashboard/CenterPanel.java
@@ -7,21 +7,82 @@ import javax.swing.table.DefaultTableModel;
 
 import java.awt.*;
 
+
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+
+import javax.swing.text.JTextComponent;
+
+
+
 public class CenterPanel extends JPanel {
 
 
-    JPanel topPanel, centerPan ,rightPanel,footerPnel;
+    JPanel topPanel, centerPan ,rightPanel;
     JLabel title;
 
     TableView table;
     JScrollPane sp;
 
+    Object[] data;
+    Object[] column;
+
+    int tbl_marginTop=0;
+
+    private Map<String,JTextField> txts;
+
+
+
+    private void initTexts(){
+        txts = new HashMap<>();
+        //600
+        for(int i = 1; i<=table.getColumns().size();i++)
+        {
+            // make bounds of textFealds to have 3 txt in a row 
+            int x = (i-1)%3;
+            int y = (i-1)/3;
+            JTextField txt = new JTextField();
+            String title =table.getColumns().get(i-1);
+            // set Placeholder
+            TextPrompt tp = new TextPrompt(title, txt);
+
+            // txt.addFocusListener(new FocusAdapter() {
+
+               
+            //     @Override
+            //     public void focusGained(FocusEvent e) {
+            //         txt.setText(title);
+            //     }
+
+            //     @Override
+            //     public void focusLost(FocusEvent e) {
+            //        txt.setText("");
+            //     }
+            // });
+
+            txt.setBounds(10+x*200, 10+y*40, 180, 30);
+            
+            txts.put(table.getColumns().get(i-1),txt);
+
+            tbl_marginTop=(10+y*40)+40;
+        }
+
+        // add txts to center Panel
+        for(int i = 0; i<table.getColumns().size();i++)
+        {
+            centerPan.add(txts.get(table.getColumns().get(i)));
+        }
+    }
+
 
     private void initTableView(){
         table = new TableView(new DefaultTableModel());
 
-        Object[] data =new Object[]{"101","Majdar","Soufiane","SoufianeMJ","sf%&4lcj456mfpsFGhd","GB748","sofiane@mail.com","06 0000000","1023"};
-        String column[]={"ID","Nom","Prenom","Login","PssWord","CIN","Email","Tele","ID Agance"};
+        data =new Object[]{"101","Majdar","Soufiane","SoufianeMJ","sf%&4lcj456mfpsFGhd","GB748","sofiane@mail.com","06 0000000","1023"};
+        column=new Object[]{"ID","Nom","Prenom","Login","PssWord","CIN","Email","Tele","ID Agance"};
 
         // Create a couple of columns 
         table.addColumns(column);
@@ -33,7 +94,7 @@ public class CenterPanel extends JPanel {
         }
 
         sp=new JScrollPane(table);
-        sp.setBounds(10, 100, 580, 200);
+        
 
     }
 
@@ -48,22 +109,25 @@ public class CenterPanel extends JPanel {
 
         centerPan= new JPanel();
         centerPan.setBackground(new Color(247, 249, 252));
-        centerPan.setBounds(10, 50, 600, 320);
+        centerPan.setBounds(10, 50, 600, 360);
         centerPan.setLayout(null);
+
+
 
         initTableView();
         centerPan.add(sp);
 
+        initTexts();
+        sp.setBounds(10, tbl_marginTop, 580, 200);
+        System.out.println(tbl_marginTop);
 
-        footerPnel= new JPanel();
-        footerPnel.setBackground(new Color(199, 201, 215));
-        footerPnel.setBounds(10, 370, 590, 40);
+
 
     }
 
     private void initLabels(){
         title= new JLabel();
-        title.setText("<HTML><U>Formulaire Client</U><HTML>");
+        title.setText("<HTML><U>Formulaire Admin</U><HTML>");
         title.setForeground(new Color(58, 51, 122));
         title.setBorder(new EmptyBorder(6,0,0,0));
         title.setFont(new Font("Arale",Font.BOLD,17));
@@ -82,7 +146,7 @@ public class CenterPanel extends JPanel {
 
         add(centerPan);
   
-        add(footerPnel);
+     
     }
 }
 
--- src/View/Dashboard/TableView.java
+++ src/View/Dashboard/TableView.java
@@ -1,7 +1,16 @@
 package View.Dashboard;
 
+import javax.swing.*;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.JTableHeader;
+
+import java.awt.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+
 
 public class TableView extends JTable {
 
@@ -13,6 +22,17 @@ public class TableView extends JTable {
     public TableView(DefaultTableModel model){
         super(model);
         index=0;
+
+        Font font = new Font("Verdana", Font.PLAIN, 10);
+        setFont(font);
+        setRowHeight(20);
+
+
+        JTableHeader tableHeader = this.getTableHeader();
+        tableHeader.setBackground(new Color(103, 114, 226));
+        tableHeader.setForeground(Color.white);
+        Font headerFont = new Font("Verdana", Font.PLAIN, 12);
+        tableHeader.setFont(headerFont);
     }
 
 
@@ -20,10 +40,10 @@ public class TableView extends JTable {
 
 
     // ADD Columns
-    public void addColumns(String[] columns)
+    public void addColumns(Object[] columns)
     {
         // add "Index" to Comluns
-        String[] newColumns = new String[columns.length+1];
+        Object[] newColumns = new Object[columns.length+1];
         newColumns[0]="Index";
         for(int i=0;i<columns.length;i++)
         {
@@ -33,6 +53,7 @@ public class TableView extends JTable {
 
         DefaultTableModel model = (DefaultTableModel) this.getModel();
         model.setColumnIdentifiers(columns);
+
     }
 
     // ADD Row
@@ -52,6 +73,17 @@ public class TableView extends JTable {
         index++;
     }
  
+    // get columns names
+    public List<String> getColumns()
+    {
+        int columnsCount = this.getColumnCount();
+        List<String> columns = new java.util.ArrayList<String>();
+        for(int i=0;i<columnsCount;i++)
+        {
+            columns.add(this.getColumnName(i));
+        }
+        return columns;
+    }
     
     
 }
